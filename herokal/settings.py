2bf16bfcc9e7805166f0617e73d5320b18aee50e
Dan Drinkard <dan.drinkard@gmail.com>
2015-08-30 17:21:51 -0400

    support contexts where settings module is not directly on pythonpath

--- herokal/settings.py
+++ herokal/settings.py
@@ -2,6 +2,19 @@ import os
 import re
 import json
 import sys
+import importlib
+
+
+def apply_setting(setting, value, **kwargs):
+    if re.search(r'^[A-Z][A-Z0-9_]+$', setting):
+        if kwargs.get('parse_json') is True:
+            try:
+                setattr(sys.modules[__name__], setting, json.loads(value))
+            except:
+                setattr(sys.modules[__name__], setting, value)
+        else:
+            setattr(sys.modules[__name__], setting, value)
+
 
 if 'DEBUG' in os.environ.keys():
     # Load .env file and
@@ -14,14 +27,13 @@ if 'DEBUG' in os.environ.keys():
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     for setting, value in os.environ.iteritems():
-        if re.search(r'^[A-Z][A-Z0-9_]+$', setting):
-            try:
-                setattr(sys.modules[__name__], setting, json.loads(value))
-            except:
-                setattr(sys.modules[__name__], setting, value)
+        apply_setting(setting, value, parse_json=True)
 else:
     try:
-        from .local_settings import *
+        local_settings = importlib.import_module('..local_settings',
+                                                 os.environ['DJANGO_SETTINGS_MODULE'])
+        for name in dir(local_settings):
+            apply_setting(name, getattr(local_settings, name))
     except ImportError as e:
         print("""Caught %s trying to import local_settings. Please make sure
                  local_settings.py exists and is free of errors.""")
@@ -32,5 +44,7 @@ try:
     del re
     del json
     del sys
+    del local_settings
+    del apply_setting
 except:
     pass
